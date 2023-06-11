95df4bc853454e3a58dd500af0661f1c09d9cf51
Domenica-Munoz <56518381+Domenica-Munoz@users.noreply.github.com>
2023-06-10 23:18:26 -0500

    Actualizacion de personas

--- src/main/java/ec/fin/ups/controller/CPersona.java
+++ src/main/java/ec/fin/ups/controller/CPersona.java
@@ -3,6 +3,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -29,7 +30,7 @@ public class CPersona {
 	}
 	
 	@GetMapping ("/obtenerPersona/{cedula}")
-	public Persona listar(@PathVariable String cedula) {
+	public Persona buscar(@PathVariable String cedula) {
 		Persona persona=personaService.findByCedula(cedula);
 		return persona;
 	}
@@ -38,6 +39,12 @@ public class CPersona {
 	public List<Persona> listar() {
 		List<Persona> clientes=personaService.listar();
 		return clientes;
+	}
+	
+	@PostMapping ("/editarPersona")
+	public String editar(@RequestBody Persona p) {
+		personaService.save(p);
+		return "ok";
 	}
 	
 }
