abf9d3eca98a2d00642a3fb4ba696bd2072525c3
Esteban Novillo <estebannovilloq@gmail.com>
2023-06-14 22:23:08 -0500

    Swagger actualizado

--- src/main/java/ec/fin/ups/controller/CEstadoCuenta.java
+++ src/main/java/ec/fin/ups/controller/CEstadoCuenta.java
@@ -3,6 +3,8 @@ package ec.fin.ups.controller;
 import ec.fin.ups.Utils.JWTUtil;
 import ec.fin.ups.interfaceServices.IEstadoCuentaService;
 import ec.fin.ups.modelo.EstadoCuenta;
+import io.swagger.v3.oas.annotations.Operation;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -27,12 +29,23 @@ public class CEstadoCuenta {
         return usuarioId != null;
     }
 
+    @Operation(
+			summary = "Estados de cuenta",
+			description = "Este servicio obtiene los estados de cuenta del cliente.\n" +
+					"        No se requieren parámetros adicionales en la solicitud."
+	)
     @GetMapping("/estadoCuenta")
     public List<EstadoCuenta> estadosCuenta(@RequestHeader(value = "Authorization") String token){
         if (!validateToken(token)){ return new ArrayList<>(); }
         return service.listarEstadosCuenta();
     }
 
+
+    @Operation(
+			summary = "Actualizar estado de cuenta",
+			description = "Este servicio permite actualizar un estado de cuenta.\n" +
+					"        Requiere autorización."
+	)
     @PostMapping("/estadoCuenta")
     public String guardarEstadoCuenta(
             @RequestHeader(value = "Authorization") String token,
@@ -42,7 +55,11 @@ public class CEstadoCuenta {
         service.guardarEstadoCuenta(estadoCuenta);
         return "OK";
     }
-
+    @Operation(
+			summary = "Actualizar estado de cuenta",
+			description = "Este servicio permite actualizar un estado de cuenta.\n" +
+					"        Requiere autorización. Se recibe el ID desde el header."
+	)
     @GetMapping("/estadoCuenta/{id}")
     public EstadoCuenta estadoCuentaById(
             @RequestHeader(value = "Authorization") String token,
